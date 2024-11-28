55a2920efd1b274e3b27199bd882ff89f533a15b
mojtaba-jsx <mojtaba.jsx@gmail.com>
2024-11-28 08:41:35 +0330

    add

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-server.listen(4000, () => {
+server.listen(3000, () => {
   console.log("JSON Server is running");
 });
+
+
+
